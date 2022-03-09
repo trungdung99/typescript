5c3b91e6022fdb9eb22fa67a3b3fb8079320bcab
Trung Dung <dungptph14123@fpt.edu.vn>
2022-03-09 14:03:39 +0700

    update

---
+++ disc/app.js
@@ -0,0 +1,18 @@
+"use strict";
+// function sum(a: number, b:number){
+//     return a+b;
+// }
+// const a = 10;
+// const b = 20;
+// const result = sum(10, 20)
+// console.log(result);
+const person = {
+    id: 1,
+    name: "Dung",
+    age: 22,
+};
+const persons = [
+    { id: 1, name: "dffdf", age: 21, status: true },
+    { id: 2, name: "sdsadsad", age: 20, status: false },
+];
+console.log(persons);
