dad457e8574828a1d5c00f5a25dc69a11bbf21d9
Trung Dung <dungptph14123@fpt.edu.vn>
2022-03-11 14:11:23 +0700

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,17 +5,24 @@ import ShowInfo from '../components/ShowInfo'
 import Item from '../components/Item'
 
 function App() {
-  const products = [
-    {id: 1, name: "Sản phẩm 1"},
-    {id: 2, name: "Sản phẩm 2"},
-    {id: 3, name: "Sản phẩm 3"}
-  ]
 
   const [count, setCount] = useState(0);
+  const [products, setProduct] = useState([
+    [
+      {id: 1, name: "Sản phẩm 1"},
+      {id: 2, name: "Sản phẩm 2"},
+      {id: 3, name: "Sản phẩm 3"}
+    ]
+  ])
+  const removeItem = (id) => {
+    const newsProduct = products.filter(item => item.id !== id);
+    setProduct(newsProduct);
+  }
   return (
     <div className="App">
-      Count: {count} <button onClick={() => setCount(count + 1)}>Click</button>
-      {products.map((index ,item) => <Item key={index} data={item} />)}
+      {products.map((item, index) =>  <div>{item.name}
+      <button onClick={() => removeItem(item.id)}>Remove</button></div>
+      )}
       <ShowInfo name="Dung" />
     </div>
   )
